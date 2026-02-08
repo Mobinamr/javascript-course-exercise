c58611b44b4dcce2d8b14496fffe939b0672bca6
Mobin Amiri <amirialimobin@gmail.com>
2026-02-08 15:34:23 +0100

    added date function

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -3,7 +3,14 @@ const todoList = [];
 function addTodo() {
   const inputElement =  document.querySelector('.js-name-input2');
   const name = inputElement.value;
-  todoList.push(name);
+
+  const dateInputElement = document.querySelector('.js-due-date');
+  const dueDate = dateInputElement.value;
+
+  todoList.push({
+    name: name,
+    dueDate: dueDate
+  });
   console.log(todoList);
   inputElement.value = '';
   renderTodoList();
@@ -15,10 +22,13 @@ function renderTodoList() {
   let todoListHTML = '';
 
   for (let i = 0; i < todoList.length; i++) {
-    const todo = todoList[i];
+    const todoObject = todoList[i];
+    //const name = todoObject.name;
+    //const { dueDate } = todoObject;
+    const { name, dueDate } = todoObject;
     const html = `
     <p>
-    ${todo} 
+    ${name} ${dueDate}
     <button onclick="
       todoList.splice(${i}, 1);
       renderTodoList();
